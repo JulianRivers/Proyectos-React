2fcd83c45b3644f2245c20eccc6afbd626d2144e
Julian Riveros <juliancamilorifo@ufps.edu.co>
2023-07-28 15:09:49 -0500

    comienzo GifApp usando la api de Giphy

---
+++ 05-GifApp/src/GifApp.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
+
+export const GifApp = () => {
+
+  const [categories, setCategories] = useState(['One punch', 'One piece']);
+
+  const onAddCategory = (newCategory) => {
+    if(categories.includes(newCategory)) return;
+    setCategories([newCategory, ...categories])
+  }
+
+  return (
+    <>
+      <h1>
+        GifApp
+      </h1>
+      <AddCategory onNewCategory={onAddCategory} />
+      {
+        categories.map( (category) => (
+          <GifGrid key={category} category={category}/>
+        ))
+      }
+    </>
+  )
+}
